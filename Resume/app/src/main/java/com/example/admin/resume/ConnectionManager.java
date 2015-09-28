be25a72b0fe6ffdebf23e3cdbe5eda3a0b2d9be6
Ranjit Ghodke <rghodke@ucsd.edu>
2015-09-28 00:03:31 -0700

    Added more screens to implement
    Fixed some minor bugs as well as implemented more activities that need
    finishing up.

---
+++ Resume/app/src/main/java/com/example/admin/resume/ConnectionManager.java
@@ -0,0 +1,43 @@
+package com.example.admin.resume;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class ConnectionManager extends AppCompatActivity {
+
+    ListView connections;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection_manager);
+        connections = (ListView)findViewById(R.id.ConnectionListView);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_connection_manager, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
